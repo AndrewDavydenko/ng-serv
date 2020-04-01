937f888e66b7445a3ebfe885f936fbf13b2837a2
AndrewDavydenko <drun2098@gmail.com>
2020-04-01 12:02:52 +0300

    Feature: data parsing [ref:no-ref]

---
+++ data/parse-string.ts
@@ -0,0 +1,45 @@
+import * as mongoose from 'mongoose';
+// const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const url =
+'mongodb://127.0.0.1:27017';
+const products = require('./output/json-products.json');
+const categories = require('./output/json-categories.json');
+const subcategories = require('./output/json-sub-categories.json');
+
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true },
+  // tslint:disable-next-line:no-any
+  async (_err: any, db: any) => {
+    const dbo = db.db('heroku_smfd2t77');
+    // tslint:disable-next-line:no-any
+    products.map((elem: any) => {
+      elem._id = mongoose.Types.ObjectId(elem._id);
+      elem.subCategory = mongoose.Types.ObjectId(elem.subCategory);
+      // tslint:disable-next-line:no-any
+      dbo.collection('products').insertOne(elem);
+    });
+    // tslint:disable-next-line:no-any
+    categories.map((elem: any) => {
+      elem._id = mongoose.Types.ObjectId(elem._id);
+      dbo.collection('categories').insertOne(elem);
+    });
+    // tslint:disable-next-line:no-any
+    subcategories.map((elem: any) => {
+      elem._id = mongoose.Types.ObjectId(elem._id);
+      elem.category = mongoose.Types.ObjectId(elem.category);
+      dbo.collection('subcategories').insertOne(elem);
+    });
+    const anotherCat = await dbo
+      .collection('categories')
+      .insertOne({ name: 'Прочее' });
+    await dbo.collection('subcategories').insertOne({
+      category: mongoose.Types.ObjectId(anotherCat._id),
+      name: 'Без категории',
+    });
+    // tslint:disable-next-line:no-console
+    console.log('done');
+    db.close();
+  }
+);
